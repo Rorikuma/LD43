1e7f75e3c065c8110f9359297af498d5bbf53b69
RahmKota3 <38017498+RahmKota3@users.noreply.github.com>
2018-12-02 15:02:33 +0100

    Day & night cycle

---
+++ AztecSacrifice/Assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public float DayTime = 240;
+    public float MaxAlpha = 0.5f;
+
+    int day = 1;
+    int halfDay = 0;
+    float time = 0;
+
+    public SpriteRenderer NightSprite;
+
+    private void Start()
+    {
+        NightSprite.color = new Color(NightSprite.color.r, NightSprite.color.g, NightSprite.color.b, 0);
+    }
+
+    void ControlDayAndNight()
+    {
+        if (halfDay % 2 == 0)
+        {
+            time += Time.deltaTime;
+
+            if (time >= DayTime)
+            {
+                time = DayTime;
+                halfDay += 1;
+                day += 1;
+            }
+        }
+        else
+        {
+            time -= Time.deltaTime;
+
+            if (time <= 0)
+            {
+                time = 0;
+                halfDay += 1;
+                day += 1;
+            }
+        }
+
+        float alpha = (MaxAlpha * time) / DayTime;
+        NightSprite.color = new Color(NightSprite.color.r, NightSprite.color.g, NightSprite.color.b, alpha);
+    }
+
+    private void Update()
+    {
+        ControlDayAndNight();
+    }
+
+}
