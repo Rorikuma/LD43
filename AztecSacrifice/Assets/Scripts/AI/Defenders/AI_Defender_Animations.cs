a18f741d53baf82a4f17999b641dcc28e4249d5c
RahmKota3 <38017498+RahmKota3@users.noreply.github.com>
2018-12-04 01:41:45 +0100

    test

---
+++ AztecSacrifice/Assets/Scripts/AI/Defenders/AI_Defender_Animations.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Defender_Animations : MonoBehaviour {
+
+    public Animator KidAnim;
+    public Animator AdultAnim;
+    public Animator OldAnim;
+
+
+
+}
